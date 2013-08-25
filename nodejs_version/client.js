116f808a1c5152168a02380e4157cd6e0b0451c6
Kinepoo <aleksander.elvemo@gmail.com>
2013-08-25 15:53:42 +0200

    Added image loading

--- nodejs_version/client.js
+++ nodejs_version/client.js
@@ -1,11 +1,30 @@
 var socket;
 
+var canvas;
+var canvasContext;
+var canvasStage;
+var lionSpriteSheet;
+
 $(document).ready(function(){
 	var connectTo = 'http://'+location.host.substring(0,location.host.indexOf(':'));
 	console.log('Connecting to ' + connectTo);
 	socket = io.connect(connectTo);
+
+	canvas = document.getElementById('gamescreen');
+	canvasContext = canvas.getContext("2d");
+	canvasStage = new createjs.Stage(document.getElementById("gamescreen"));
+
+	loadImages();
 });
 
 function sendDataToServer(trigger, data){
 	socket.emit(trigger, data);
+}
+
+function loadImages(){
+	lionSpriteSheet = new Image();
+	lionSpriteSheet.src = 'lion.png';
+	lionSpriteSheet.image.onload = function(){
+    	console.log('Loaded lionSpriteSheet');
+  	}
 }
