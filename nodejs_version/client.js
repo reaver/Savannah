62597a5b7eb3fadb705eb173b0f51b8ed797a630
reaver <reaver.rikku@gmail.com>
2013-08-25 16:24:15 +0200

    qdasd

--- nodejs_version/client.js
+++ nodejs_version/client.js
@@ -4,16 +4,24 @@ var canvas;
 var canvasContext;
 var canvasStage;
 var lionSpriteSheet;
+var antilopeSpriteSheet;
+var animalID;
 
 $(document).ready(function(){
 	var connectTo = 'http://'+location.host.substring(0,location.host.indexOf(':'));
 	console.log('Connecting to ' + connectTo);
 	socket = io.connect(connectTo);
 
+	socket.on("animalID", function(data) {
+	  	console.log('Data ' + data);
+		animalID = data;
+	}.bind(this));
+	
 	canvas = document.getElementById('gamescreen');
 	canvasContext = canvas.getContext("2d");
 	canvasStage = new createjs.Stage(document.getElementById("gamescreen"));
 
+	renderBackground();
 	loadImages();
 });
 
@@ -24,7 +32,19 @@ function sendDataToServer(trigger, data){
 function loadImages(){
 	lionSpriteSheet = new Image();
 	lionSpriteSheet.src = 'lion.png';
-	lionSpriteSheet.image.onload = function(){
-    	console.log('Loaded lionSpriteSheet');
-  	}
+	antilopeSpriteSheet = new Image();
+	antilopeSpriteSheet.src = 'antilope.png';
+	//lionSpriteSheet.image.onload = function(){
+    //	console.log('Loaded lionSpriteSheet');
+  	//}
+}
+
+function renderBackground(){
+	var color = "#E0C31D";
+	canvasContext.fillStyle=color;
+	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function renderPlayer(){
+	
 }
